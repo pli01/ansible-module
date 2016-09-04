23515d13d1e229e11585ad0400f2baec59e4fda7
pli <philippe.libat@free.fr>
2016-09-04 19:25:29 +0200

    try

--- scripts/get_drive.sh
+++ scripts/get_drive.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 access_token="ya29.Ci9UAxD7nMohPqIsv4L6QtFiJlbpO8UDwZX4O39RSy_LAuYtmPBQkM4I8hEQK9q_wA"
 
-curl -o /tmp/simple.tar.gz -J -L  -H 'GData-Version: 3.0' -H "Authorization: Bearer $access_token" https://www.googleapis.com/drive/v2/files/0BxavvYDwQGVnSWQzWkJxMDVnV3c?alt=media&acknowledgeAbuse=true
+curl -o /tmp/simple.tar.gz -J -L  -H 'GData-Version: 3.0' -H "Authorization: Bearer $access_token" "https://www.googleapis.com/drive/v2/files/0BxavvYDwQGVnSWQzWkJxMDVnV3c?alt=media&acknowledgeAbuse=true"
+sleep 2
